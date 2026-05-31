988b1317fa242396deb4870f858b093c9ac98d9a
IhTiYaR0 <ytpolyus@gmail.com>
2026-05-31 23:54:01 +0500

    Работает

--- main.py
+++ main.py
@@ -904,5 +904,5 @@ async def submit_flag(request: Request, ch_id: int, flag: str = Form(...), db: S
     return RedirectResponse(url=f"/challenge/{ch_id}", status_code=303)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
 
